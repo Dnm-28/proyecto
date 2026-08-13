b2981aecd54d291c3051d57313005cb721629bda
Dnm-28 <68307988+Dnm-28@users.noreply.github.com>
2026-08-12 19:09:00 -0600

    Primer commit del portafolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// Scroll suave
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
